1676f7fa2513955c33e553a2389ead171d66c6cd
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2020-05-30 23:52:17 +0900

    fix maxProfit

---
+++ easy-collections/arrays/besttimetobuy/maxProfit2.js
@@ -0,0 +1,10 @@
+var maxProfit = function(prices) {
+  var result = 0;
+  for (var i=1; i<prices.length; i++) {
+    if (prices[i] > prices[i-1]) {
+      result += prices[i] - prices[i-1];
+    }
+  }
+  return result;
+};
+
