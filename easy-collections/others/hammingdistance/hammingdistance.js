ac0a400384856961dd8eafdf964372cec76139e1
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2020-06-12 00:27:16 +0900

    solve hamming distance

---
+++ easy-collections/others/hammingdistance/hammingdistance.js
@@ -0,0 +1,16 @@
+var hammingWeight = function(bits) {
+    bits = (bits & 0x55555555) + (bits >> 1 & 0x55555555);
+    bits = (bits & 0x33333333) + (bits >> 2 & 0x33333333);
+    bits = (bits & 0x0f0f0f0f) + (bits >> 4 & 0x0f0f0f0f);
+    bits = (bits & 0x00ff00ff) + (bits >> 8 & 0x00ff00ff);
+    return (bits & 0x0000ffff) + (bits >>16 & 0x0000ffff);
+};
+
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function(x, y) {
+    return hammingWeight(x ^ y);
+};
