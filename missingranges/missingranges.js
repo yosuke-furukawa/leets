759b4ca23f7ee070547c7abf863a694c1a79233b
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2020-06-20 00:35:07 +0900

    solve missing ranges

---
+++ missingranges/missingranges.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} lower
+ * @param {number} upper
+ * @return {string[]}
+ */
+var findMissingRanges = function(nums, lower, upper) {
+  const output = [];
+  let pre = lower;
+  for (const num of nums) {
+    if (num > pre) {
+      output.push([pre, num - 1]);
+    }
+    pre = num + 1;
+  }
+  if (upper >= pre) {
+    output.push([pre, upper]);
+  }
+  return output.map(([s, e]) => (s === e ? `${s}` : `${s}->${e}`));
+};
