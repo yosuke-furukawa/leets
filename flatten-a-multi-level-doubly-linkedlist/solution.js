2eaa4293f353bd0013301691fb53edfb22b43448
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2020-07-10 22:46:52 +0900

    solve flatten a multi level doubly linkedlist

---
+++ flatten-a-multi-level-doubly-linkedlist/solution.js
@@ -0,0 +1,41 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,prev,next,child) {
+ *    this.val = val;
+ *    this.prev = prev;
+ *    this.next = next;
+ *    this.child = child;
+ * };
+ */
+
+var traverse = function(node, next) {
+  let n = node;
+  while (n?.next != null || n?.child != null) {
+    if (n.child) {
+      let { next, child } = n;
+      n.next = child;
+      child.prev = n;
+      n.child = null;
+      traverse(child, next);
+      if (next) n = next.prev;
+    }
+    n = n.next;
+  }
+  if (next) {
+    n.next = next;
+    next.prev = n;
+  }
+} 
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var flatten = function(head) {
+  var h = head;
+  if (h == null) {
+    return null;
+  }
+  traverse(h, null);
+  return head;
+};
