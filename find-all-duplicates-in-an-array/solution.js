dea64fe379ea12f1f7855bca520aa38fadc5957a
Yosuke Furukawa <yosuke.furukawa@gmail.com>
2020-08-06 17:51:21 +0900

    solve find all duplicates in an array

--- find-all-duplicates-in-an-array/solution.js
+++ find-all-duplicates-in-an-array/solution.js
@@ -5,7 +5,6 @@
 var findDuplicates = function(nums) {
   const res = [];
   for (let i = 0; i < nums.length; i++) {
-    console.log(nums);
     const idx = Math.abs(nums[i]) - 1;
     if (nums[idx] < 0) res.push(idx + 1);
     else nums[idx] = -nums[idx];
