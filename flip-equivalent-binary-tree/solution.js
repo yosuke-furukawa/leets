4eae21049b13e9eecefcb1d9da8bf933f65dc93e
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-08-09 07:56:32 +0900

    solve flip equivalent binary tree

---
+++ flip-equivalent-binary-tree/solution.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {boolean}
+ */
+var flipEquiv = function(root1, root2) {
+  if (root1 == null && root2 == null) {
+    return true;
+  }
+  if (root1 == null || root2 == null) {
+    return false;
+  }
+  if (root1.val !== root2.val) {
+    return false;
+  }
+  
+  return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)) || (flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+};
