6ccc50f67c145d1fc3fbb676d0321712bdbde244
古川　陽介 <01015773@CA6585.local>
2020-08-18 17:34:50 +0900

    test

--- fenwicktree.js
+++ fenwicktree.js
@@ -53,7 +53,6 @@ function main() {
   var ft = new FenwickTree([
     [3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]
   ]);
-
   console.log(ft.query2d(2,1,4,3));
   ft.update(3,2,2);
   console.log(ft.query2d(2,1,4,3));
