f06a172ba4990cfd6e15281a2cfd307ec4da5c7f
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-09-08 22:49:22 +0900

    solve flatten binary tree to linkedlist

---
+++ flatten-binary-tree-to-linked-list/solution2.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function(root, parent) {
+  if (root == null) {
+    return;
+  }
+
+  var node = root;
+
+  while (node != null) {
+
+    // If the node has a left child
+    if (node.left != null) {
+
+      // Find the rightmost node
+      var rightmost = node.left;
+      while (rightmost.right != null) {
+        rightmost = rightmost.right;
+      }
+
+      // rewire the connections
+      rightmost.right = node.right;
+      node.right = node.left;
+      node.left = null;
+    }
+
+    // move on to the right side of the tree
+    node = node.right;
+  }
+};
