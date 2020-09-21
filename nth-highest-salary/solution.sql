908133599a49121f742ce75884934b3e4da5df74
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-09-21 22:21:47 +0900

    solve nth highest salary

---
+++ nth-highest-salary/solution.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      SELECT e1.Salary
+      FROM (SELECT DISTINCT Salary FROM Employee) e1
+      WHERE (SELECT COUNT(*) FROM (SELECT DISTINCT Salary FROM Employee) e2 WHERE e2.Salary > e1.Salary) = N - 1      
+      
+      LIMIT 1
+  );
+END
