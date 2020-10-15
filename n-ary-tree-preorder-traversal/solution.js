cd1a7c48a350e7046a9a33115ffe34db91a91831
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-10-15 22:27:10 +0900

    solve n-ary tree preorder traversal

---
+++ n-ary-tree-preorder-traversal/solution.js
@@ -0,0 +1,25 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var preorder = function(root) {
+  var results = [];
+  var helper = (node) => {
+    if (node?.children == null) {
+      return;
+    }
+    results.push(node.val);
+    
+    node.children.forEach((n) => helper(n));
+  }
+  helper(root);
+  return results;
+};
