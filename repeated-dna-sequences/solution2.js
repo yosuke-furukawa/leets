598f5a9079d2574df48a90fa59ffb3a75de18086
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-10-17 19:34:47 +0900

    solve repeated dna sequences3

--- repeated-dna-sequences/solution2.js
+++ repeated-dna-sequences/solution2.js
@@ -3,13 +3,13 @@
  * @return {string[]}
  */
 var findRepeatedDnaSequences = function(s) {
-  let sequenceMap = new Map();
-  let result = [];
+  const sequenceMap = new Map();
+  const result = [];
   if (s.length < 10) {
     return result;
   }
   for (let i = 0; i < s.length - 9; i++) {
-    let substring = s.slice(i, i+10);
+    const substring = s.substring(i, i+10);
     if (sequenceMap.has(substring)) {
       if (sequenceMap.get(substring) < 2) {
         sequenceMap.set(substring, 2);
