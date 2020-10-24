b1f289b6cab122b95801c5c615461003f192cc33
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-10-25 01:00:21 +0900

    update trie.js

--- trie.js
+++ trie.js
@@ -6,55 +6,55 @@ class TrieNode {
   }
 }
 
-var Trie = function() {
-  this.root = new TrieNode();
-};
-
-Trie.prototype.insert = function(word) {
-  var node = this.root;
-  for (const char of word) {
-    if (node.children.has(char)) {
-      node = node.children.get(char);
-    } else {
-      var newNode = new TrieNode();
-      node.children.set(char, newNode);
-      node = newNode; 
-    }
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
   }
-  node.word = word;
-};
-
-/**
- * Returns if the word is in the trie. 
- * @param {string} word
- * @return {boolean}
- */
-Trie.prototype.search = function(word) {
-  var node = this.root
-  for (const char of word) {
-    if (node.children.has(char)) {
-      node = node.children.get(char);
-    } else {
-      return false;
+  insert(word) {
+    var node = this.root;
+    for (const char of word) {
+      if (node.children.has(char)) {
+        node = node.children.get(char);
+      } else {
+        var newNode = new TrieNode();
+        node.children.set(char, newNode);
+        node = newNode; 
+      }
     }
+    node.word = word;
   }
-  return node.word === word;
-};
-
-/**
- * Returns if there is any word in the trie that starts with the given prefix. 
- * @param {string} prefix
- * @return {boolean}
- */
-Trie.prototype.startsWith = function(prefix) {
-  var node = this.root
-  for (const char of prefix) {
-    if (node.children.has(char)) {
-      node = node.children.get(char);
-    } else {
-      return false;
+  /**
+   * Returns if the word is in the trie. 
+   * @param {string} word
+   * @return {boolean}
+   */
+  search(word) {
+    var node = this.root
+    for (const char of word) {
+      if (node.children.has(char)) {
+        node = node.children.get(char);
+      } else {
+        return false;
+      }
     }
+    return node.word === word;
   }
-  return true;
-};
+
+  /**
+   * Returns if there is any word in the trie that starts with the given prefix. 
+   * @param {string} prefix
+   * @return {boolean}
+   */
+  startsWith(prefix) {
+    var node = this.root
+    for (const char of prefix) {
+      if (node.children.has(char)) {
+        node = node.children.get(char);
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+}
 
