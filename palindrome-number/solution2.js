7660f3bbb1ddb978adf7726960d78fcef3c38d5e
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-12-25 17:52:31 +0900

    solve palindrome number

---
+++ palindrome-number/solution2.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+  if(x < 0 || (x % 10 == 0 && x != 0)) {
+      return false;
+  }
+
+  var revertedNumber = 0;
+  while(x > revertedNumber) {
+    revertedNumber = revertedNumber * 10 + x % 10;
+    x = Math.floor(x / 10);
+  }
+  return x === revertedNumber || x === Math.floor(revertedNumber/10);
+};
