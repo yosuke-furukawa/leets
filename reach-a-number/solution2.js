cd555032f81df6d3108a94a5450231f16074ab2c
yosuke-furukawa <yosuke.furukawa@gmail.com>
2020-12-28 21:20:59 +0900

    solve reach a number

---
+++ reach-a-number/solution2.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} target
+ * @return {number}
+ */
+var reachNumber = function(target) {
+  target = Math.abs(target);
+  var k = 0;
+  while (target > 0)
+    target -= ++k;
+  return target % 2 == 0 ? k : k + 1 + k%2;
+};
