aee3fe7d98efe150f10b7c50fe4d69ef90f3c8f6
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-01-15 20:05:08 +0900

    [rust] refactor Get Maximum in Generated Array

--- get-maximum-in-generated-array/src/main.rs
+++ get-maximum-in-generated-array/src/main.rs
@@ -2,8 +2,8 @@ struct Solution {}
 
 impl Solution {
     pub fn get_maximum_generated(n: i32) -> i32 {
-        let mut nums: Vec<i32> = vec![];
-        for i in 0..n + 1 {
+        let mut nums: Vec<i32> = Vec::with_capacity((n+1) as usize);
+        for i in 0..n+1 {
             nums.push(match i {
                 0 => 0,
                 1 => 1,
@@ -16,5 +16,5 @@ impl Solution {
 }
 
 fn main() {
-    println!("{}", Solution::get_maximum_generated(7));
+    println!("{}", Solution::get_maximum_generated(100));
 }
