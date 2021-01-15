afc02adffe23336495373850a521dcbc377a968f
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-01-15 18:56:19 +0900

    [rust] solve Get Maximum in Generated Array

---
+++ get-maximum-in-generated-array/src/main.rs
@@ -0,0 +1,20 @@
+struct Solution {}
+
+impl Solution {
+    pub fn get_maximum_generated(n: i32) -> i32 {
+        let mut nums: Vec<i32> = vec![];
+        for i in 0..n + 1 {
+            nums.push(match i {
+                0 => 0,
+                1 => 1,
+                i if i % 2 == 0 => nums[(i / 2) as usize],
+                _ => nums[(i / 2) as usize] + nums[(i / 2 + 1) as usize],
+            });
+        }
+        *nums.iter().max().unwrap()
+    }
+}
+
+fn main() {
+    println!("{}", Solution::get_maximum_generated(7));
+}
