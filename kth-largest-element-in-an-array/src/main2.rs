72a71cd17ea50499e747dea57a00a0735bc05486
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-01-16 20:06:12 +0900

    [rust] solve using binheap

---
+++ kth-largest-element-in-an-array/src/main2.rs
@@ -0,0 +1,25 @@
+use std::collections::BinaryHeap;
+
+impl Solution {
+    pub fn find_kth_largest(nums: Vec<i32>, mut k: i32) -> i32 {
+        let mut heap: BinaryHeap<i32> = BinaryHeap::new();
+        for n in nums {
+            heap.push(n);
+        }
+
+        let mut n = Some(0 as i32);
+        while k > 0 {
+            n = heap.pop();
+            k -= 1;
+        }
+
+        n.unwrap()
+    }
+}
+
+struct Solution;
+
+fn main() {
+    println!("{}", Solution::find_kth_largest(vec![3,2,1,5,6,4], 2));
+    println!("{}", Solution::find_kth_largest(vec![3,2,3,1,2,4,5,5,6], 4));
+}
