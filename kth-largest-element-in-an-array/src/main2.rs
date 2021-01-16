a5ceca8f088a3ba378d105674223c427918722ce
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-01-16 20:10:53 +0900

    refactor using Binheap from

--- kth-largest-element-in-an-array/src/main2.rs
+++ kth-largest-element-in-an-array/src/main2.rs
@@ -2,10 +2,7 @@ use std::collections::BinaryHeap;
 
 impl Solution {
     pub fn find_kth_largest(nums: Vec<i32>, mut k: i32) -> i32 {
-        let mut heap: BinaryHeap<i32> = BinaryHeap::new();
-        for n in nums {
-            heap.push(n);
-        }
+        let mut heap = BinaryHeap::from(nums);
 
         let mut n = Some(0 as i32);
         while k > 0 {
@@ -16,7 +13,6 @@ impl Solution {
         n.unwrap()
     }
 }
-
 struct Solution;
 
 fn main() {
