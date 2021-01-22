63ef90398b3ed47903f0ed0f6603b81905085f4c
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-01-22 20:11:20 +0900

    [js]solve Print Immutable Linked List in Reverse

---
+++ print-immutable-linked-list-in-reverse/solution.js
@@ -0,0 +1,32 @@
+/**
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function ImmutableListNode() {
+ *    @ return {void}
+ *    this.printValue = function() { // print the value of this node.
+ *       ...
+ *    }; 
+ *
+ *    @return {ImmutableListNode}
+ *    this.getNext = function() { // return the next node.
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {ImmutableListNode} head
+ * @return {void}
+ */
+var printLinkedListInReverse = function(head) {
+  var stack = [];
+  var h = head;
+  while (h != null) {
+    stack.unshift(h);
+    h = h.getNext();
+  }
+  
+  for (let n of stack) {
+    n.printValue();
+  }
+};
