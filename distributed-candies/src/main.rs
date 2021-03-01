7a2978030bc4fb5e8d364eb1bacd503d7686b0b7
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-03-01 21:56:17 +0900

    [rust]refactor distribute candies

--- distributed-candies/src/main.rs
+++ distributed-candies/src/main.rs
@@ -2,8 +2,8 @@ use std::collections::HashSet;
 
 impl Solution {
     pub fn distribute_candies(candy_type: Vec<i32>) -> i32 {
-        let max_candy: i32 = (candy_type.len() / 2) as i32;
-        let kinds: i32 = candy_type.into_iter().collect::<HashSet<i32>>().len() as i32;
+        let max_candy = (candy_type.len() / 2) as i32;
+        let kinds = candy_type.into_iter().collect::<HashSet<i32>>().len() as i32;
         if max_candy < kinds {
             return max_candy;
         }
