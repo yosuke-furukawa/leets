d0aff3e0ab95f8b81e4d97fa6be8c1c06bb68205
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-03-01 21:38:44 +0900

    [rust] solve distributed candies

---
+++ distributed-candies/src/main.rs
@@ -0,0 +1,18 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn distribute_candies(candy_type: Vec<i32>) -> i32 {
+        let max_candy: i32 = (candy_type.len() / 2) as i32;
+        let kinds: i32 = candy_type.into_iter().collect::<HashSet<i32>>().len() as i32;
+        if max_candy < kinds {
+            return max_candy;
+        }
+        kinds
+    }
+}
+
+struct Solution;
+
+fn main() {
+    println!("{}", Solution::distribute_candies(vec![1, 1, 2, 2, 3, 3]));
+}
