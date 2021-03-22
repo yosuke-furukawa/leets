9f934fbd83d1362b8d28f9745864b1116d2b83d5
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-03-22 18:06:05 +0900

    [rust] refactor find smallest common element in all rows

--- find-smallest-common-element-in-all-rows/src/main.rs
+++ find-smallest-common-element-in-all-rows/src/main.rs
@@ -1,19 +1,18 @@
 use std::collections::HashSet;
-use std::iter::FromIterator;
 
 impl Solution {
     pub fn smallest_common_element(mat: Vec<Vec<i32>>) -> i32 {
-        let init:HashSet<i32> = mat.clone().into_iter().flatten().collect();
-        let intersection = mat.into_iter().map(HashSet::from_iter).try_fold(init, |set, temp| {
-            match (set, temp) {
-                (set, _) if set.is_empty() => None,
-                (set, temp) => Some(set.intersection(&temp).copied().collect()),
-            }
-        });
-        if intersection.is_none() {
+        let mut set: HashSet<i32> = mat.first().unwrap().iter().cloned().collect();
+        for s in mat.iter().skip(1) {
+            set = set
+                .intersection(&s.iter().cloned().collect())
+                .cloned()
+                .collect();
+        }
+        if set.is_empty() {
             return -1;
         }
-        let mut arr: Vec<i32> = intersection.unwrap().iter().copied().collect();
+        let mut arr: Vec<i32> = set.iter().cloned().collect();
         arr.sort_unstable();
         *arr.first().unwrap()
     }
@@ -21,6 +20,26 @@ impl Solution {
 
 struct Solution;
 
+macro_rules! grid {
+    ( $([$( $x: expr ), *]), *) => {
+        {
+            vec![
+                $(
+                    vec![$($x), *],
+                )*
+            ]
+        }
+    };
+}
+
 fn main() {
-    println!("{}", Solution::smallest_common_element(vec![vec![1,2,3,4,5],vec![2,4,5,8,10],vec![3,5,7,9,11],vec![1,3,5,7,9]]));
+    println!(
+        "{}",
+        Solution::smallest_common_element(grid![
+            [1, 2, 3, 4, 5],
+            [2, 4, 5, 8, 10],
+            [3, 5, 7, 9, 11],
+            [1, 3, 5, 7, 9]
+        ])
+    );
 }
