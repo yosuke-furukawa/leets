cda0f4f226f5e872da7570ad860f8732503173fc
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-03-22 19:49:38 +0900

    [rust] refactor vowel spellchecker

--- vowel-spellchecker/src/main.rs
+++ vowel-spellchecker/src/main.rs
@@ -4,11 +4,6 @@ use std::collections::HashSet;
 impl Solution {
     fn replace_vowels(s: String) -> String {
         let mut result = s;
-        result = result.replace("a", "*");
-        result = result.replace("i", "*");
-        result = result.replace("u", "*");
-        result = result.replace("e", "*");
-        result = result.replace("o", "*");
         result = result.replace("A", "*");
         result = result.replace("I", "*");
         result = result.replace("U", "*");
@@ -18,36 +13,31 @@ impl Solution {
     }
     pub fn spellchecker(wordlist: Vec<String>, queries: Vec<String>) -> Vec<String> {
         let words: HashSet<String> = wordlist.iter().cloned().collect();
-        let mut capitalized_suggests: HashMap<String, Vec<String>> = HashMap::new();
-        let mut vowel_suggests: HashMap<String, Vec<String>> = HashMap::new();
+        let mut capitalized_suggests: HashMap<String, String> = HashMap::new();
+        let mut vowel_suggests: HashMap<String, String> = HashMap::new();
         let mut result = vec![];
 
         for word in wordlist.iter() {
             let capital_key = word.to_ascii_uppercase();
             capitalized_suggests
                 .entry(capital_key.clone())
-                .or_default()
-                .push(word.clone());
-            let vowel_key = Self::replace_vowels(capital_key.clone());
-            vowel_suggests
-                .entry(vowel_key.clone())
-                .or_default()
-                .push(word.clone());
+                .or_insert_with(|| word.clone());
+            let vowel_key = Self::replace_vowels(capital_key);
+            vowel_suggests.entry(vowel_key).or_insert_with(|| word.clone());
         }
 
         for query in queries {
-            let key = Self::replace_vowels(query.clone()).to_ascii_uppercase();
+            let key = Self::replace_vowels(query.to_ascii_uppercase());
             if words.contains(&query) {
                 result.push(query);
             } else if let Some(s) = capitalized_suggests.get(&query.to_ascii_uppercase()) {
-                result.push(s.first().unwrap().clone());
+                result.push(s.clone());
             } else if let Some(s) = vowel_suggests.get(&key) {
-                result.push(s.first().unwrap().clone());
+                result.push(s.clone());
             } else {
                 result.push("".to_string());
             }
         }
-
         result
     }
 }
