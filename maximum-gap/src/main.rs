14fc169fa1bbefc36bf9c3b61e991f3066638d0c
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-05-30 21:39:07 +0900

    [rust] solve maximum gap

--- maximum-gap/src/main.rs
+++ maximum-gap/src/main.rs
@@ -1,12 +1,42 @@
 impl Solution {
     pub fn maximum_gap(nums: Vec<i32>) -> i32 {
-        let mut nums = nums;
-        nums.sort_unstable();
-        let mut max = 0;
-        for window in nums.windows(2) {
-            max = max.max(window[1] - window[0]);
+        if nums.len() <= 1 {
+            return 0;
+        } else if nums.len() == 2 {
+            return (nums[1] - nums[0]).abs();
         }
-        max
+        let n = nums.len();
+        use std::i32::{MAX, MIN};
+        let mut maxs = vec![MIN; n];
+        let mut mins = vec![MAX; n];
+        let max = *nums.iter().max().unwrap();
+        let min = *nums.iter().min().unwrap();
+        if max == min {
+            return 0;
+        }
+
+        let avggap = (max - min) as f64 / (n as f64 - 1.0);
+        for &v in nums.iter() {
+            let idx = ((v - min) as f64 / avggap) as usize;
+            mins[idx] = mins[idx].min(v);
+            maxs[idx] = maxs[idx].max(v);
+        }
+
+        let premax = maxs[0];
+        mins.into_iter()
+            .zip(maxs)
+            .skip(1)
+            .scan(premax, |premax, (cur_min, cur_max)| {
+                if cur_min <= cur_max {
+                    let premax_ = *premax;
+                    *premax = cur_max;
+                    Some(cur_min - premax_)
+                } else {
+                    Some(MIN)
+                }
+            })
+            .max()
+            .unwrap()
     }
 }
 
