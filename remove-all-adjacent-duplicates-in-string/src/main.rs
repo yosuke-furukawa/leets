6757d96ee58f56573cd7ee86abbb50943d17af1f
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-06-28 16:48:14 +0900

    [rust] solve remove all adjacent duplicates in string

---
+++ remove-all-adjacent-duplicates-in-string/src/main.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn remove_duplicates(s: String) -> String {
+        let mut v = vec![];
+        for c in s.chars() {
+            if v.is_empty() {
+                v.push(c);
+                continue;
+            }
+            let last = v.last().unwrap();
+            if last == &c {
+                v.pop();
+                continue;
+            }
+            v.push(c);
+        }
+        v.into_iter()
+            .fold(String::new(), |acc, cur| acc + cur.to_string().as_str())
+    }
+}
+
+struct Solution;
+
+fn main() {
+    println!("{}", Solution::remove_duplicates("abbaca".to_string()));
+}
