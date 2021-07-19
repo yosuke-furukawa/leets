3a37350e886060b1996b74b6ea278f7d1d7f6df7
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-07-19 16:38:55 +0900

    [rust] solve lowest common ancestor of a binary search tree

---
+++ lowest-common-ancestor-of-a-binary-search-tree/src/main.rs
@@ -0,0 +1,92 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    pub fn lowest_common_ancestor(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        p: Option<Rc<RefCell<TreeNode>>>,
+        q: Option<Rc<RefCell<TreeNode>>>,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        let pval = p.unwrap().borrow().val;
+        let qval = q.unwrap().borrow().val;
+        let mut head = root;
+        while let Some(node) = head.clone() {
+            let n = node.borrow();
+            let parent_val = n.val;
+            if pval > parent_val && qval > parent_val {
+                head = n.right.clone();
+            } else if pval < parent_val && qval < parent_val {
+                head = n.left.clone();
+            } else {
+                return head;
+            }
+        }
+        None
+    }
+}
+
+struct Solution;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn to_tree(vec: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+    use std::collections::VecDeque;
+    let head = Some(Rc::new(RefCell::new(TreeNode::new(vec[0].unwrap()))));
+    let mut queue = VecDeque::new();
+    queue.push_back(head.as_ref().unwrap().clone());
+
+    for children in vec[1..].chunks(2) {
+        let parent = queue.pop_front().unwrap();
+        if let Some(v) = children[0] {
+            parent.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(v))));
+            queue.push_back(parent.borrow().left.as_ref().unwrap().clone());
+        }
+        if children.len() > 1 {
+            if let Some(v) = children[1] {
+                parent.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(v))));
+                queue.push_back(parent.borrow().right.as_ref().unwrap().clone());
+            }
+        }
+    }
+    head
+}
+
+macro_rules! tree {
+    () => {
+        None
+    };
+    ($($e:expr),*) => {
+        {
+            let vec = vec![$(stringify!($e)), *];
+            let vec = vec.into_iter().map(|v| v.parse::<i32>().ok()).collect::<Vec<_>>();
+            to_tree(vec)
+        }
+    };
+    ($($e:expr,)*) => {(tree![$($e),*])};
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        Solution::lowest_common_ancestor(
+            tree![6, 2, 8, 0, 4, 7, 9, null, null, 3, 5],
+            Some(Rc::new(RefCell::new(TreeNode::new(2)))),
+            Some(Rc::new(RefCell::new(TreeNode::new(8))))
+        )
+    );
+}
