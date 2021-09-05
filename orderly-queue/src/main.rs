e1819fadc3e8f960f5c5dcd1ad67a0cac8c72680
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-09-05 21:04:10 +0900

    [rust] give up... solve orderly queue

--- orderly-queue/src/main.rs
+++ orderly-queue/src/main.rs
@@ -1,31 +1,21 @@
-use std::collections::BTreeSet;
-use std::collections::VecDeque;
-
 impl Solution {
     pub fn orderly_queue(s: String, k: i32) -> String {
-        let mut set: BTreeSet<String> = BTreeSet::new();
-        if k as usize > s.len() / 2 {
-            let mut chars: Vec<char> = s.chars().collect();
-            chars.sort_unstable();
-            return chars
-                .iter()
-                .fold(String::new(), |acc, cur| acc + &cur.to_string());
-        }
-        let mut queue: VecDeque<String> = VecDeque::new();
-        queue.push_back(s);
-        while !queue.is_empty() {
-            let s = queue.pop_front().unwrap();
-            for i in 0..k as usize {
-                let t = s.get(0..i).unwrap().to_string()
-                    + s.get(i + 1..).unwrap()
-                    + s.get(i..i + 1).unwrap();
-                if !set.contains(&t) {
-                    set.insert(t.clone());
-                    queue.push_back(t);
+        if k == 1 {
+            let mut ans = s.clone();
+            for i in 0..s.len() {
+                let t = s.get(i..).unwrap().to_string() + s.get(0..i).unwrap();
+                if t.cmp(&ans) == std::cmp::Ordering::Less {
+                    ans = t;
                 }
             }
+            ans
+        } else {
+            let mut chars: Vec<char> = s.chars().collect();
+            chars.sort_unstable();
+            chars
+                .into_iter()
+                .fold(String::new(), |acc, cur| acc + &cur.to_string())
         }
-        set.into_iter().next().unwrap()
     }
 }
 
