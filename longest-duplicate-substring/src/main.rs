f73ee3604e17b9666d4b07ac7bd351eeb4f4f690
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-10-30 23:05:45 +0900

    [rust] give up... longest duplicate substring

--- longest-duplicate-substring/src/main.rs
+++ longest-duplicate-substring/src/main.rs
@@ -1,30 +1,49 @@
+use std::collections::HashMap;
+
+const M: u64 = (1 << 31) - 1;
+const B: u64 = 257;
+
 impl Solution {
     pub fn longest_dup_substring(s: String) -> String {
-        let mut max_len = 0;
-        let mut str_start = 0;
-
-        let str = s.as_bytes();
-        if str.iter().all(|v| *v == str[0]) {
-            return s[1..s.len()].to_string();
+        let s: &[u8] = s.as_bytes();
+        let mut hashs: Vec<u64> = Vec::with_capacity(s.len() + 1);
+        let mut hash = 0;
+        hashs.push(hash);
+        for &u in s.iter() {
+            hash = (hash * B + u as u64) % M;
+            hashs.push(hash);
         }
 
-        let mut start = 0;
-        for end in 1..s.len() {
-            if max_len >= (end - start) {
-                break;
-            }
-
-            let str_temp = &s[start..end];
-
-            if s[(start + 1)..s.len()].contains(str_temp) && str_temp.len() > max_len {
-                str_start = start;
-                max_len = str_temp.len();
+        let (mut l, mut r) = (0, s.len());
+        let mut answer: usize = 0;
+        while l < r {
+            let m = l + (r - l) / 2;
+            if let Some(a) = Solution::helper(s, m, &hashs) {
+                answer = a;
+                l = m + 1
             } else {
-                start += 1;
+                r = m
             }
         }
-
-        s[str_start..(str_start + max_len)].to_string()
+        s[answer..answer + l - 1]
+            .iter()
+            .map(|&u| u as char)
+            .collect()
+    }
+    fn helper(s: &[u8], len: usize, hashs: &[u64]) -> Option<usize> {
+        let b = (0..len as u64).fold(1, |acc, _| (acc * B) % M);
+        let mut hm: HashMap<u64, usize> = HashMap::new();
+        for i in 0..s.len() - len + 1 {
+            let h = (hashs[i + len] + M - hashs[i] * b % M) % M;
+            if let Some(j) = hm.get(&h) {
+                if (0..len).all(|k| s[i + k] == s[j + k]) {
+                    return Some(*j);
+                }
+            } else {
+                hm.insert(h, i);
+            }
+        }
+        None
     }
 }
 
