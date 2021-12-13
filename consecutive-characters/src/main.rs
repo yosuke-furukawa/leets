d349d245be85dc348f4afbaa360ebccf49557cd8
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-12-13 22:12:44 +0900

    [rust] solve consecutive characters

---
+++ consecutive-characters/src/main.rs
@@ -0,0 +1,20 @@
+impl Solution {
+    pub fn max_power(s: String) -> i32 {
+        let mut arr = vec![1; s.len()];
+        let chars: Vec<char> = s.chars().collect();
+        let mut max = 0;
+        for i in 1..arr.len() {
+            if chars[i] == chars[i - 1] {
+                arr[i] = arr[i - 1] + 1;
+                max = max.max(arr[i]);
+            }
+        }
+        max
+    }
+}
+
+struct Solution;
+
+fn main() {
+    println!("{}", Solution::max_power("leetcode".to_string()));
+}
