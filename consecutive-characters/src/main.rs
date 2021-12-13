e1f34129e39970ed67081b22431ec4a06ebd2f26
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-12-13 22:13:29 +0900

    fix: consecutive characters

--- consecutive-characters/src/main.rs
+++ consecutive-characters/src/main.rs
@@ -2,7 +2,7 @@ impl Solution {
     pub fn max_power(s: String) -> i32 {
         let mut arr = vec![1; s.len()];
         let chars: Vec<char> = s.chars().collect();
-        let mut max = 0;
+        let mut max = 1;
         for i in 1..arr.len() {
             if chars[i] == chars[i - 1] {
                 arr[i] = arr[i - 1] + 1;
