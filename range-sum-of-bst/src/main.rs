bf1397b965d20ad04b18aa959534199e3d5b9072
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-12-14 14:15:21 +0900

    [rust] solve range sum of bst

---
+++ range-sum-of-bst/src/main.rs
@@ -0,0 +1,82 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    fn helper(sum: &mut i32, low: i32, high: i32, node: Option<&Rc<RefCell<TreeNode>>>) {
+        if let Some(n) = node {
+            let v = n.borrow().val;
+            if v >= low && v <= high {
+                *sum += v;
+            }
+            Self::helper(sum, low, high, n.borrow().left.as_ref());
+            Self::helper(sum, low, high, n.borrow().right.as_ref());
+        }
+    }
+    pub fn range_sum_bst(root: Option<Rc<RefCell<TreeNode>>>, low: i32, high: i32) -> i32 {
+        let mut sum = 0;
+        Self::helper(&mut sum, low, high, root.as_ref());
+        sum
+    }
+}
+
+struct Solution;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+fn to_tree(vec: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+    use std::collections::VecDeque;
+    let head = Some(Rc::new(RefCell::new(TreeNode::new(vec[0].unwrap()))));
+    let mut queue = VecDeque::new();
+    queue.push_back(head.as_ref().unwrap().clone());
+
+    for children in vec[1..].chunks(2) {
+        let parent = queue.pop_front().unwrap();
+        if let Some(v) = children[0] {
+            parent.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(v))));
+            queue.push_back(parent.borrow().left.as_ref().unwrap().clone());
+        }
+        if children.len() > 1 {
+            if let Some(v) = children[1] {
+                parent.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(v))));
+                queue.push_back(parent.borrow().right.as_ref().unwrap().clone());
+            }
+        }
+    }
+    head
+}
+
+macro_rules! tree {
+    () => {
+        None
+    };
+    ($($e:expr),*) => {
+        {
+            let vec = vec![$(stringify!($e)), *];
+            let vec = vec.into_iter().map(|v| v.parse::<i32>().ok()).collect::<Vec<_>>();
+            to_tree(vec)
+        }
+    };
+    ($($e:expr,)*) => {(tree![$($e),*])};
+}
+
+fn main() {
+    println!(
+        "{}",
+        Solution::range_sum_bst(tree![10, 5, 15, 3, 7, null, 18], 7, 15)
+    );
+}
