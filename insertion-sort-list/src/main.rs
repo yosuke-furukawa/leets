d1602eefe405ca47c5fd405c7977a44f1e9a643f
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-12-15 20:17:02 +0900

    [rust] solve insertion sort list

---
+++ insertion-sort-list/src/main.rs
@@ -0,0 +1,46 @@
+impl Solution {
+    pub fn insertion_sort_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut array = vec![];
+        let mut h = head;
+        while let Some(mut n) = h {
+            array.push(n.val);
+            h = n.next.take();
+        }
+        array.sort_unstable();
+        to_list(array)
+    }
+}
+
+struct Solution;
+
+pub fn to_list(vec: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut current = None;
+    for &v in vec.iter().rev() {
+        let mut node = ListNode::new(v);
+        node.next = current;
+        current = Some(Box::new(node));
+    }
+    current
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+macro_rules! linked {
+    ($($e:expr),*) => {to_list(vec![$($e.to_owned()), *])};
+    ($($e:expr,)*) => {to_list(vec![$($e.to_owned()), *])};
+}
+
+fn main() {
+    println!("{:?}", Solution::insertion_sort_list(linked![4, 2, 1, 3]));
+}
