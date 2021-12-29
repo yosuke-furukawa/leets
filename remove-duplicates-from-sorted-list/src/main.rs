1828d23511eb18ed9e466ef3f8c38b6c221f47d0
yosuke-furukawa <yosuke.furukawa@gmail.com>
2021-12-29 19:12:09 +0900

    [rust] solve remove duplicates from sorted list

---
+++ remove-duplicates-from-sorted-list/src/main.rs
@@ -0,0 +1,51 @@
+impl Solution {
+    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if head.is_none() {
+            return head;
+        }
+        let mut h = head;
+        let mut n = h.as_mut().unwrap();
+        while let Some(nn) = n.next.as_mut() {
+            if n.val == nn.val {
+                n.next = nn.next.take();
+            } else {
+                n = n.next.as_mut().unwrap();
+            }
+        }
+        h
+    }
+}
+
+struct Solution;
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+pub fn to_list(vec: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut current = None;
+    for &v in vec.iter().rev() {
+        let mut node = ListNode::new(v);
+        node.next = current;
+        current = Some(Box::new(node));
+    }
+    current
+}
+
+macro_rules! linked {
+    ($($e:expr),*) => {to_list(vec![$($e.to_owned()), *])};
+    ($($e:expr,)*) => {to_list(vec![$($e.to_owned()), *])};
+}
+
+fn main() {
+    println!("{:?}", Solution::delete_duplicates(linked![1, 1, 2]));
+}
