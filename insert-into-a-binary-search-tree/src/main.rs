005980587282b7190db470db6cc0ab4af57bc79b
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-01-12 18:28:21 +0900

    [rust] solve insert into a binary search tree

---
+++ insert-into-a-binary-search-tree/src/main.rs
@@ -0,0 +1,91 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+impl Solution {
+    fn helper(node: &mut Option<&Rc<RefCell<TreeNode>>>, val: i32) {
+        if let Some(n) = node {
+            if val < n.borrow().val {
+                if n.borrow().left.is_some() {
+                    Self::helper(&mut n.borrow().left.as_ref(), val)
+                } else {
+                    n.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(val))))
+                }
+            } else {
+                if n.borrow().right.is_some() {
+                    Self::helper(&mut n.borrow().right.as_ref(), val)
+                } else {
+                    n.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(val))))
+                }
+            }
+        }
+    }
+    pub fn insert_into_bst(
+        root: Option<Rc<RefCell<TreeNode>>>,
+        val: i32,
+    ) -> Option<Rc<RefCell<TreeNode>>> {
+        if root.is_none() {
+            return Some(Rc::new(RefCell::new(TreeNode::new(val))));
+        }
+        Self::helper(&mut root.as_ref(), val);
+        root
+    }
+}
+
+struct Solution;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+fn to_tree(vec: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+    use std::collections::VecDeque;
+    let head = Some(Rc::new(RefCell::new(TreeNode::new(vec[0].unwrap()))));
+    let mut queue = VecDeque::new();
+    queue.push_back(head.as_ref().unwrap().clone());
+
+    for children in vec[1..].chunks(2) {
+        let parent = queue.pop_front().unwrap();
+        if let Some(v) = children[0] {
+            parent.borrow_mut().left = Some(Rc::new(RefCell::new(TreeNode::new(v))));
+            queue.push_back(parent.borrow().left.as_ref().unwrap().clone());
+        }
+        if children.len() > 1 {
+            if let Some(v) = children[1] {
+                parent.borrow_mut().right = Some(Rc::new(RefCell::new(TreeNode::new(v))));
+                queue.push_back(parent.borrow().right.as_ref().unwrap().clone());
+            }
+        }
+    }
+    head
+}
+
+macro_rules! tree {
+    () => {
+        None
+    };
+    ($($e:expr),*) => {
+        {
+            let vec = vec![$(stringify!($e)), *];
+            let vec = vec.into_iter().map(|v| v.parse::<i32>().ok()).collect::<Vec<_>>();
+            to_tree(vec)
+        }
+    };
+    ($($e:expr,)*) => {(tree![$($e),*])};
+}
+
+fn main() {
+    println!("{:?}", Solution::insert_into_bst(tree![4, 2, 7, 1, 3], 5));
+}
