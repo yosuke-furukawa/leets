34e0421245acc234c326f28daba94032e0978fb5
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-02-07 09:44:52 +0900

    [rust] use one line code in find the difference

--- find-the-difference/src/main.rs
+++ find-the-difference/src/main.rs
@@ -1,23 +1,6 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn find_the_difference(s: String, t: String) -> char {
-        let mut map = HashMap::new();
-        for c in s.chars() {
-            *map.entry(c).or_insert(0) += 1;
-        }
-        for c in t.chars() {
-            if let Some(v) = map.get_mut(&c) {
-                if *v == 0 {
-                    return c;
-                } else {
-                    *v -= 1;
-                }
-            } else {
-                return c;
-            }
-        }
-        ' '
+        (s + &t).chars().fold(0, |sum, c| sum ^ c as u8) as char
     }
 }
 
