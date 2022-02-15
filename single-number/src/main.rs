69d947e6e38bbe5dbf1e3db892713d73ef9d385f
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-02-15 09:46:00 +0900

    [rust] solve single number using oneliner

--- single-number/src/main.rs
+++ single-number/src/main.rs
@@ -1,16 +1,6 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        let mut set = HashSet::new();
-        for num in nums.iter() {
-            if set.contains(num) {
-                set.remove(num);
-            } else {
-                set.insert(*num);
-            }
-        }
-        *set.iter().next().unwrap()
+        nums.into_iter().reduce(|a, b| a ^ b).unwrap_or(0)
     }
 }
 
