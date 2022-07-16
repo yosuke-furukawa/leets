b39105205b2f8e6783fbe9d98be7d1164325bb30
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-07-16 23:40:53 +0900

    [rust] update out of boundary paths

--- out-of-boundary-paths/src/main.rs
+++ out-of-boundary-paths/src/main.rs
@@ -4,8 +4,7 @@ impl Solution {
         let mut dp = vec![vec![0; n as usize]; m as usize];
         dp[start_row as usize][start_column as usize] = 1;
         let mut count = 0;
-        for _ in 1..=max_move {
-            // println!("{:?}", dp);
+        for _ in 0..=max_move {
             let mut temp = vec![vec![0; n as usize]; m as usize];
             for i in 0..m as usize {
                 for j in 0..n as usize {
@@ -21,13 +20,14 @@ impl Solution {
                     if j == 0 {
                         count = (count + dp[i][j]) % limit;
                     }
-                    temp[i][j] = ((if i > 0 { dp[i - 1][j] } else { 0 }
-                        + if i < m as usize - 1 { dp[i + 1][j] } else { 0 })
-                        % limit
-                        + (if j > 0 { dp[i][j - 1] } else { 0 }
-                            + if j < n as usize - 1 { dp[i][j + 1] } else { 0 })
-                            % limit)
-                        % limit;
+                    for d in [0, 1, 0, !0, 0].windows(2) {
+                        let row = (i as i32).wrapping_add(d[0]);
+                        let col = (j as i32).wrapping_add(d[1]);
+                        if (0..m).contains(&row) && (0..n).contains(&col) {
+                            temp[row as usize][col as usize] =
+                                (temp[row as usize][col as usize] + dp[i][j]) % limit;
+                        }
+                    }
                 }
             }
             dp = temp;
