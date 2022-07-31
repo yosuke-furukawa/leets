4213a3b23bc6b5a4b1e500147d76c6421fb77bf2
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-07-31 23:02:45 +0900

    [rust] update range sum query mutable

--- range-sum-query-mutable/src/main.rs
+++ range-sum-query-mutable/src/main.rs
@@ -1,26 +1,81 @@
-struct NumArray {
-    array: Vec<i32>,
+struct BitTree {
+    tree: Vec<i32>,
+    data: Vec<i32>,
+    n: usize,
 }
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
+impl BitTree {
+    fn new(n: usize) -> Self {
+        let tree = vec![0; n];
+        let data = vec![0; n];
+        let n = n;
+        BitTree { tree, data, n }
+    }
+
+    fn get(&self, i: usize) -> i32 {
+        self.data[i]
+    }
+
+    fn sum(&self, i: usize) -> i32 {
+        let mut res = 0;
+        let down_iter = std::iter::successors(Some(i), |&i| {
+            let j = i & (i + 1);
+            if j > 0 {
+                Some(j - 1)
+            } else {
+                None
+            }
+        });
+        for j in down_iter {
+            res += self.tree[j];
+        }
+        res
+    }
+
+    fn add(&mut self, i: usize, v: i32) {
+        self.data[i] += v;
+        let n = self.n;
+        let up_iter = std::iter::successors(Some(i), |&i| {
+            let j = i | (i + 1);
+            if j < n {
+                Some(j)
+            } else {
+                None
+            }
+        });
+        for j in up_iter {
+            self.tree[j] += v;
+        }
+    }
+}
+
+struct NumArray {
+    bit_tree: BitTree,
+}
+
 impl NumArray {
     fn new(nums: Vec<i32>) -> Self {
-        Self { array: nums }
+        let n = nums.len();
+        let mut bit_tree = BitTree::new(n);
+        for i in 0..n {
+            bit_tree.add(i, nums[i]);
+        }
+        NumArray { bit_tree }
     }
 
-    fn update(&mut self, index: i32, val: i32) {
-        self.array[index as usize] = val;
+    fn update(&mut self, i: i32, val: i32) {
+        let i = i as usize;
+        self.bit_tree.add(i as usize, val - self.bit_tree.get(i))
     }
 
-    fn sum_range(&self, left: i32, right: i32) -> i32 {
-        self.array
-            .iter()
-            .skip(left as usize)
-            .take(right as usize - left as usize + 1)
-            .sum()
+    fn sum_range(&self, i: i32, j: i32) -> i32 {
+        let i = i as usize;
+        let j = j as usize;
+        if i > 0 {
+            self.bit_tree.sum(j) - self.bit_tree.sum(i - 1)
+        } else {
+            self.bit_tree.sum(j)
+        }
     }
 }
 
