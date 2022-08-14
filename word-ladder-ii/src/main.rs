2c3d5ffb171f1e44d15de98f664b47b50c1a0c90
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-08-14 12:15:23 +0900

    [rust] give up... solve word ladder ii

--- word-ladder-ii/src/main.rs
+++ word-ladder-ii/src/main.rs
@@ -1,104 +1,151 @@
 use std::collections::HashMap;
-use std::collections::VecDeque;
-use std::i32::MAX;
+use std::collections::LinkedList;
+
+fn word_to_bits(word: &[u8]) -> u32 {
+    let mut res = 0;
+    for (i, c) in word.iter().enumerate() {
+        res |= ((*c - b'a' + 1) as u32) << (i as u32 * 5);
+    }
+    res
+}
+
+fn clear_loc(word: u32, loc: usize) -> u32 {
+    let mask = u32::MAX - (0x1F << (loc * 5));
+    word & mask
+}
+
+#[derive(Default, Debug)]
+struct ResultNode {
+    nexts: LinkedList<usize>,
+    level: usize,
+}
 
 impl Solution {
     pub fn find_ladders(
         begin_word: String,
         end_word: String,
-        word_list: Vec<String>,
+        mut word_list: Vec<String>,
     ) -> Vec<Vec<String>> {
-        let mut word_id = HashMap::new();
-        let mut id_word = vec![];
-        let mut id = 0;
-
-        for word in word_list.iter() {
-            if !word_id.contains_key(word) {
-                word_id.insert(word.to_string(), id);
-                id += 1;
-                id_word.push(word.to_string());
+        let len = word_list.len();
+        let n = begin_word.len();
+        let mut words = word_list
+            .iter()
+            .map(|s| word_to_bits(s.as_bytes()))
+            .collect::<Vec<_>>();
+        let mut indices = HashMap::<u32, usize>::with_capacity(len + 2);
+        for (i, word) in words.iter().cloned().enumerate() {
+            indices.insert(word, i);
+        }
+        let begin_word_ori = begin_word.clone();
+        let begin_word = word_to_bits(begin_word.as_bytes());
+        let end_word = word_to_bits(end_word.as_bytes());
+        let begin_index = *indices.entry(begin_word).or_insert_with(|| {
+            words.push(begin_word);
+            word_list.push(begin_word_ori);
+            word_list.len() - 1
+        });
+        let end_index = match indices.get(&end_word) {
+            Some(x) => *x,
+            None => return vec![],
+        };
+        let mut slots = HashMap::<u32, LinkedList<usize>>::new();
+        for (word, i) in indices.iter() {
+            let word = *word;
+            let i = *i;
+            for j in 0..n {
+                let slot = clear_loc(word, j);
+                slots.entry(slot).or_default().push_front(i);
             }
         }
 
-        if !word_id.contains_key(&end_word) {
+        if (0..n).all(|j| slots.get(&clear_loc(end_word, j)).unwrap().len() == 1)
+            || (0..n).all(|j| slots.get(&clear_loc(begin_word, j)).unwrap().len() == 1)
+        {
             return vec![];
         }
 
-        if !word_id.contains_key(&begin_word) {
-            word_id.insert(begin_word.to_string(), id);
-            id += 1;
-            id_word.push(begin_word.to_string());
-        }
-        let mut edges = vec![vec![]; id_word.len()];
+        let mut nodes = Vec::<Option<ResultNode>>::with_capacity(len + 2);
+        (0..=len + 2).for_each(|_| nodes.push(None));
+        nodes[end_index].replace(ResultNode {
+            nexts: Default::default(),
+            level: 0,
+        });
+        let qcap = len + 3;
+        let mut q = vec![0usize; qcap];
+        let mut head = 0;
+        let mut tail = 1;
+        let mut min_length = usize::MAX;
+        q[0] = end_index;
 
-        for i in 0..id_word.len() {
-            for j in (i + 1)..id_word.len() {
-                if Self::transform_check(
-                    id_word.get(i).unwrap().as_str(),
-                    id_word.get(j).unwrap().as_str(),
-                ) {
-                    edges[i].push(j);
-                    edges[j].push(i);
-                }
+        while head != tail {
+            let curi = q[head];
+            let cur_level = nodes[curi].as_ref().unwrap().level;
+            if cur_level >= min_length {
+                break;
             }
-        }
+            let next_level = cur_level + 1;
+            head = (head + 1) % qcap;
 
-        let dest = *word_id.get(&end_word).unwrap();
-        let begin = *word_id.get(&begin_word).unwrap();
-
-        Solution::bfs(id, begin, dest, id_word, edges)
-    }
-
-    fn bfs(
-        id: usize,
-        begin: usize,
-        dest: usize,
-        dict: Vec<String>,
-        edges: Vec<Vec<usize>>,
-    ) -> Vec<Vec<String>> {
-        let mut ans = vec![];
-        let mut cost = vec![MAX; id];
-
-        let mut queue = VecDeque::new();
-        queue.push_back(vec![begin]);
-        cost[begin] = 0;
-
-        while let Some(now) = queue.pop_front() {
-            if let Some(last) = now.last() {
-                if *last == dest {
-                    let mut tmp = vec![];
-                    for index in now.iter() {
-                        tmp.push(dict[*index as usize].to_string());
+            for j in 0..n {
+                for prev_i in slots
+                    .get(&clear_loc(words[curi], j))
+                    .unwrap()
+                    .iter()
+                    .cloned()
+                {
+                    if prev_i == begin_index {
+                        min_length = next_level;
                     }
-
-                    ans.push(tmp);
-                } else {
-                    for i in 0..edges[*last as usize].len() {
-                        let to = *edges[*last as usize].get(i).unwrap();
-                        if cost[*last as usize] < cost[to as usize] {
-                            cost[to as usize] = cost[*last as usize] + 1;
-                            let mut tmp = vec![];
-                            tmp.extend(now.iter().copied());
-                            tmp.push(to);
-                            queue.push_back(tmp);
+                    if prev_i == curi {
+                        continue;
+                    }
+                    let prev_node = &mut nodes[prev_i];
+                    match prev_node {
+                        x @ None => {
+                            x.replace(ResultNode {
+                                nexts: LinkedList::from([curi]),
+                                level: next_level,
+                            });
+                            q[tail] = prev_i;
+                            tail = (tail + 1) % qcap;
                         }
-                    }
+                        Some(ref mut node) if node.level == next_level => {
+                            node.nexts.push_front(curi);
+                        }
+                        _ => continue,
+                    };
                 }
             }
         }
 
-        ans
+        let mut results = vec![];
+        make_results(
+            begin_index,
+            &word_list,
+            &nodes,
+            &mut vec![word_list[begin_index].clone()],
+            &mut results,
+        );
+        results
     }
+}
 
-    fn transform_check(s1: &str, s2: &str) -> bool {
-        let mut diff = 0;
-        for (c1, c2) in s1.chars().zip(s2.chars()) {
-            if c1 != c2 {
-                diff += 1;
-            }
-        }
-
-        diff == 1
+fn make_results(
+    i: usize,
+    word_list: &[String],
+    nodes: &[Option<ResultNode>],
+    result: &mut Vec<String>,
+    results: &mut Vec<Vec<String>>,
+) {
+    let node = nodes[i].as_ref().unwrap();
+    if node.nexts.is_empty() {
+        results.push(result.clone());
+        return;
+    }
+    for nexti in node.nexts.iter().cloned() {
+        result.push(word_list[nexti].clone());
+        make_results(nexti, word_list, nodes, result, results);
+        result.pop();
     }
 }
 
