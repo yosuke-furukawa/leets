43ccfcd90330b2b9fe7c58f58705c277b3c62afe
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-09-21 14:14:27 +0900

    [rust] improve perf on sum of even numbers after queries from 121ms to 26ms

--- sum-of-even-numbers-after-queries/src/main.rs
+++ sum-of-even-numbers-after-queries/src/main.rs
@@ -2,11 +2,18 @@ impl Solution {
     pub fn sum_even_after_queries(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
         let mut nums = nums;
         let mut result = vec![];
+        let mut sum = nums.iter().filter(|&x| x % 2 == 0).sum();
         for query in queries {
             let index = query[1] as usize;
             let val = query[0];
+            match (nums[index], nums[index] + val) {
+                (x, y) if x % 2 == 0 && y % 2 == 0 => sum += val,
+                (x, y) if x % 2 == 0 && y % 2 != 0 => sum -= x,
+                (x, y) if x % 2 != 0 && y % 2 == 0 => sum += y,
+                _ => (),
+            }
             nums[index] += val;
-            result.push(nums.iter().filter(|&x| x % 2 == 0).sum());
+            result.push(sum);
         }
         result
     }
