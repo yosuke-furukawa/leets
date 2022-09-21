7cd7e915b5f8969946cc951dfb2818cb3e2755b8
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-09-21 14:06:16 +0900

    [rust] solve sum of even numbers after queries

---
+++ sum-of-even-numbers-after-queries/src/main.rs
@@ -0,0 +1,34 @@
+impl Solution {
+    pub fn sum_even_after_queries(nums: Vec<i32>, queries: Vec<Vec<i32>>) -> Vec<i32> {
+        let mut nums = nums;
+        let mut result = vec![];
+        for query in queries {
+            let index = query[1] as usize;
+            let val = query[0];
+            nums[index] += val;
+            result.push(nums.iter().filter(|&x| x % 2 == 0).sum());
+        }
+        result
+    }
+}
+
+struct Solution;
+
+macro_rules! grid {
+    ( $([$( $x:expr ),*]),* ) => {
+        {
+            vec![
+                $(
+                    vec![$($x), *],
+                )*
+            ]
+        }
+    };
+}
+
+fn main() {
+    println!(
+        "{:?}",
+        Solution::sum_even_after_queries(vec![1, 2, 3, 4], grid![[1, 0], [-3, 1], [-4, 0], [2, 3]])
+    );
+}
