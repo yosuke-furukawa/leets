ca3278c57d7f3feaf8353bbb13a13a89b56b7edb
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-10-14 10:39:16 +0900

    [rust] solve delete the middle node of a linked list

---
+++ delete-the-middle-node-of-a-linked-list/src/main.rs
@@ -0,0 +1,47 @@
+impl Solution {
+    pub fn delete_middle(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut head = head;
+        let mut array = vec![];
+        while let Some(node) = head {
+            array.push(node.val);
+            head = node.next;
+        }
+        array.remove(array.len() / 2);
+        to_list(array.into_iter().collect())
+    }
+}
+
+pub fn to_list(vec: Vec<i32>) -> Option<Box<ListNode>> {
+    let mut current = None;
+    for &v in vec.iter().rev() {
+        let mut node = ListNode::new(v);
+        node.next = current;
+        current = Some(Box::new(node));
+    }
+    current
+}
+
+struct Solution;
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+macro_rules! linked {
+    ($($e:expr),*) => {to_list(vec![$($e.to_owned()), *])};
+    ($($e:expr,)*) => {to_list(vec![$($e.to_owned()), *])};
+}
+
+fn main() {
+    println!("{:?}", Solution::delete_middle(linked![1, 2, 3, 4, 5]));
+    println!("{:?}", Solution::delete_middle(linked![1, 2, 3, 4]));
+}
