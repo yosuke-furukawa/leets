1e89c459763101a6d7dfdf5ebaf698363e41929e
yosuke-furukawa <yosuke.furukawa@gmail.com>
2022-10-15 22:45:20 +0900

    [rust] solve string compression ii

---
+++ string-compression-ii/src/main.rs
@@ -0,0 +1,66 @@
+use std::collections::HashMap;
+
+impl Solution {
+    fn compressed_length(len: usize) -> usize {
+        if len <= 1 {
+            len
+        } else if len < 10 {
+            2
+        } else if len < 100 {
+            3
+        } else {
+            4
+        }
+    }
+
+    fn dp(
+        start: usize,
+        k: usize,
+        c: char,
+        m: usize,
+        memo: &mut HashMap<(usize, usize, char, usize), usize>,
+        s: &[char],
+        n: usize,
+    ) -> usize {
+        if let Some(&res) = memo.get(&(start, k, c, m)) {
+            res
+        } else {
+            let res = if start == n {
+                Self::compressed_length(m)
+            } else {
+                let mut res = std::usize::MAX;
+                if k > 0 {
+                    res = res.min(Self::dp(start + 1, k - 1, c, m, memo, s, n));
+                }
+                if s[start] == c {
+                    res = res.min(Self::dp(start + 1, k, c, m + 1, memo, s, n));
+                } else {
+                    res = res.min(
+                        Self::compressed_length(m)
+                            + Self::dp(start + 1, k, s[start], 1, memo, s, n),
+                    );
+                }
+                res
+            };
+            memo.insert((start, k, c, m), res);
+            res
+        }
+    }
+    fn get_length_of_optimal_compression(s: String, k: i32) -> i32 {
+        let mut memo: HashMap<(usize, usize, char, usize), usize> = HashMap::new();
+        let n = s.len();
+        let k = k as usize;
+        let s: Vec<char> = s.chars().collect();
+
+        Self::dp(0, k, ' ', 0, &mut memo, &s, n) as i32
+    }
+}
+
+struct Solution;
+
+fn main() {
+    println!(
+        "{}",
+        Solution::get_length_of_optimal_compression("aaabcccd".to_string(), 2)
+    );
+}
