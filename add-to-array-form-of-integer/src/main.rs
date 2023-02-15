70947f7953bc0a58904366da4e105b7861298766
yosuke-furukawa <yosuke.furukawa@gmail.com>
2023-02-15 21:57:19 +0900

    [rust] improve add to array form of integer

--- add-to-array-form-of-integer/src/main.rs
+++ add-to-array-form-of-integer/src/main.rs
@@ -1,17 +1,23 @@
 impl Solution {
     pub fn add_to_array_form(num: Vec<i32>, k: i32) -> Vec<i32> {
-        let mut n = 0;
-        for &x in &num {
-            n = n * 10 + x;
-        }
-        n += k;
-        let mut res = Vec::new();
-        if n == 0 {
-            res.push(0);
-        }
-        while n > 0 {
-            res.push(n % 10);
-            n /= 10;
+        let mut res = vec![];
+        let mut num = num;
+        num.reverse();
+        let mut carry = 0;
+        let mut k = k;
+        let mut i = 0;
+        while i < num.len() || k > 0 || carry > 0 {
+            let mut sum = carry;
+            if i < num.len() {
+                sum += num[i];
+            }
+            if k > 0 {
+                sum += k % 10;
+                k /= 10;
+            }
+            res.push(sum % 10);
+            carry = sum / 10;
+            i += 1;
         }
         res.reverse();
         res
