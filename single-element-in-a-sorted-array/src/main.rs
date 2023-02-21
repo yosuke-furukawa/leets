82ec439414d44992d130c6364e0436fbbf2100ba
yosuke-furukawa <yosuke.furukawa@gmail.com>
2023-02-21 14:52:10 +0900

    [rust] solve one lined single element in a sorted array

--- single-element-in-a-sorted-array/src/main.rs
+++ single-element-in-a-sorted-array/src/main.rs
@@ -1,28 +1,6 @@
 impl Solution {
     pub fn single_non_duplicate(nums: Vec<i32>) -> i32 {
-        let mut lo = 0;
-        let mut hi = nums.len() - 1;
-        while lo <= hi {
-            let mid = (lo + hi) / 2;
-            if mid % 2 == 0 {
-                if mid > 0 && nums[mid - 1] == nums[mid] {
-                    hi = mid - 1;
-                } else if mid < nums.len() - 1 && nums[mid + 1] == nums[mid] {
-                    lo = mid;
-                } else {
-                    return nums[mid];
-                }
-            } else {
-                if mid > 0 && nums[mid - 1] == nums[mid] {
-                    lo = mid + 1;
-                } else if mid < nums.len() - 1 && nums[mid + 1] == nums[mid] {
-                    hi = mid;
-                } else {
-                    return nums[mid];
-                }
-            }
-        }
-        nums[(lo + hi) / 2]
+        nums.iter().fold(0, |result, n| result ^ n)
     }
 }
 
